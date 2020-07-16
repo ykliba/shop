ac7267d17ec8c49fcd12a3dfa48afaee8cc56131
ykliba <mediaboxing2@gmail.com>
2020-07-16 09:19:17 +0900

    php

--- staff_add_check.php
+++ staff_add_check.php
@@ -13,8 +13,8 @@ $staff_pass = $_POST['pass'];
 $staff_pass2 = $_POST['pass2'];
 
 $staff_name = htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
-$staff_pass = htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
-$staff_pass2 = htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
+$staff_pass = htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
+$staff_pass2 = htmlspecialchars($staff_pass2,ENT_QUOTES,'UTF-8');
 
 if($staff_name == '')
 {
---
+++ staff_add_done.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>川原商店</title>
+</head> 
+<body>
+
+<?php
+
+try
+{
+
+$staff_name = $_POST['name'];
+$staff_pass = $_POST['pass'];
+
+$staff_name = htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
+$staff_pass = htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
+
+$dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+$user = 'root';
+$password = '';
+$dbh = new PDO($dsn, $user, $password);
+$dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$sql ='INSERT INTO mst_staff(name, password) VALUES (?,?)';
+$stmt = $dbh -> prepare($sql);
+$date[] = $staff_name;
+$date[] = $staff_pass;
+$stmt -> execute($date);
+
+$dbh = null;
+
+print $staff_name;
+print 'さんを追加しました。<br />';
+
+}
+catch(Exception $e)
+{
+  print 'ただいま障害により大変ご迷惑をおかけしております。';
+  exit();
+}
+
+?>
+
+<a href="staff_list.php">戻る</a>
+
+</body>
