9e184b69a3f7f59e50672c2795ba047ba4b77ad1
ykliba <mediaboxing2@gmail.com>
2020-07-18 22:13:10 +0900

    Update staff_edit.php

--- staff_edit.php
+++ staff_edit.php
@@ -13,11 +13,11 @@ try
 
 $staff_code = $_POST['staffcode'];
 
-$dsn = 'mysql:dbname = shop;host = localhost;charset = utf8';
+$dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
 $user = 'root';
 $password = 'root';
 $dbh = new PDO($dsn, $user, $password);
-$dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $sql = 'SELECT name FROM mst_staff WHERE code = ?';
 $stmt = $dbh -> prepare($sql);
