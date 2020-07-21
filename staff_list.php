77abcb66069d6a4b951d9b98cdce5514fa57937c
ykliba <mediaboxing2@gmail.com>
2020-07-21 10:38:13 +0900

    php

--- staff_list.php
+++ staff_list.php
@@ -25,7 +25,7 @@ $dbh = null;
 
 print 'スタッフ一覧<br /><br />';
 
-print '<form method="post" action="staff_edit.php">';
+print '<form method="post" action="staff_branch.php">';
 while(true)
 {
   $rec = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -37,7 +37,8 @@ while(true)
   print $rec['name'];
   print '<br />';
 }
-print '<input type="submit" value="修正">';
+print '<input type = "submit" name = "edit" value = "修正">';
+print '<input type = "submit" name = "delete" value = "削除">';
 print '</form>';
 }
 catch (Exception $e)
