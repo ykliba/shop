2e87cfd6c292d9e004e47a7ad96288db4fd22e29
ykliba <mediaboxing2@gmail.com>
2020-07-25 09:52:04 +0900

    Create pro_add_done.php

---
+++ product/pro_add_done.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>川原商店</title>
+</head> 
+<body>
+
+<?php
+
+try
+{
+
+$pro_name = $_POST['name'];
+$pro_price = $_POST['price'];
+
+$pro_name = htmlspecialchars($pro_name,ENT_QUOTES,'UTF-8');
+$pro_price = htmlspecialchars($pro_price,ENT_QUOTES,'UTF-8');
+
+$dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+$user = 'root';
+$password = 'root';
+$dbh = new PDO($dsn, $user, $password);
+$dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$sql = 'INSERT INTO mst_product(name,price) VALUES (?,?)';
+$stmt = $dbh -> prepare($sql);
+$data[] = $pro_name;
+$data[] = $pro_price;
+$stmt -> execute($data);
+
+$dbh = null;
+
+print $pro_name;
+print 'を追加しました。<br />';
+
+}
+catch(Exception $e)
+{
+  print 'ただいま障害により大変ご迷惑をおかけしております。';
+  exit();
+}
+
+?>
+
+<a href="pro_list.php">戻る</a>
+
+</body>
