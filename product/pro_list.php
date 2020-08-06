a72e0b413f3f9f717e436555c7f6f5f3813ffd20
ykliba <mediaboxing2@gmail.com>
2020-08-06 15:14:08 +0900

    php

--- product/pro_list.php
+++ product/pro_list.php
@@ -4,7 +4,7 @@ session_regenerate_id(true);
 if(isset($_SESSION['login'])==false)
 {
   print 'ログインされていません。<br />';
-  print 'a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+  print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
   exit();
 }
 else
